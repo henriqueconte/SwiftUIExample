7444eee1ecb0eae4bb5a95946f27dd649b5bdb23
henriqueconte <henriqueconte6@gmail.com>
2020-04-01 16:09:31 -0300

    added project

---
+++ SwiftUIExample/SwiftUIExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIExample
+//
+//  Created by Henrique Figueiredo Conte on 01/04/20.
+//  Copyright © 2020 Henrique Figueiredo Conte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
